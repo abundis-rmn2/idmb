0c6f1d22db7def74354d09f4563b5bbd2cb84cde
abundis-rmn2 <abundiscomunicacion@gmail.com>
2022-07-24 17:05:36 -0500

    NR

--- py/prueba.py
+++ py/prueba.py
@@ -1,1 +1,9 @@
-Archivo vacio
+import os
+from instagrapi import Client
+
+
+cl = Client()
+user_id_scrapp = '1724903928'
+
+print(f'información del id: {user_id_scrapp}')
+print(cl.user_info(user_id_scrapp))
