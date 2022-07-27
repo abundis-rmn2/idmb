dcf6de38e8ea95b9fe3b8d7a6687e932017dbc9c
abundis-rmn2 <abundiscomunicacion@gmail.com>
2022-07-26 23:30:55 -0500

    NR

--- py/ftp_test.py
+++ py/ftp_test.py
@@ -2,14 +2,14 @@ import ftplib
 import os
 import json
 
-f = open("../config.json")
+f = open("config.json")
 config = json.load(f)
 
 ftp_server = ftplib.FTP(config["FTP"]["hostname"],config["FTP"]["username"],config["FTP"]["password"])
 
 ftp_server.encoding = "utf-8"
 
-filename = "../config.json"
+filename = "config.json"
 
 with open(filename, "rb") as file:
     ftp_server.storbinary(f"STOR {filename}",file)
