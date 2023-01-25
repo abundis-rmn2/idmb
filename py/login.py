04ca3f84c7f2eff1e36fc734d42de3d22fac0ac1
abundis-rmn2 <abundis+github@rmn2.com>
2023-01-25 16:23:54 -0600

    -username record & added timeline & news check for app behavior and barredora param -added login by session as automatic feature

--- py/login.py
+++ py/login.py
@@ -2,6 +2,9 @@ import argparse
 from instagrapi import Client
 import ftplib
 import json
+import os.path
+import time
+
 
 parser = argparse.ArgumentParser(description='Paso de parámetros')
 #El parámetro 1 es obligatorio que sea un entero
@@ -15,11 +18,42 @@ print(params.IG_username)
 print(params.IG_password)
 print(params.IG_proxy)
 
+#revisar si existe el archivo session.json
+session = './session.json'
+check_file = os.path.isfile(session)
+print(check_file)
+if check_file == True:
+    print("session.json exists")
+    import timeline
+    print(params.IG_username, " if IG timeline its printed means the session is ok")
+    exit()
+
+c = open("config.json")
+config = json.load(c)
+ftp_server = ftplib.FTP(config["FTP"]["hostname"],config["FTP"]["username"],config["FTP"]["password"])
+ftp_server.encoding = "utf-8"
+ftp_server.cwd('session_log')
+ftp_server.dir()
+print (ftp_server.nlst())
+if f"{params.IG_username}.json" in ftp_server.nlst():
+    # use FTP's RETR command to download the file
+    #f"STOR {params.IG_username}.json"
+    print("User has session.json in FTP downloading")
+    with open("session.json", 'wb') as f:
+        ftp_server.retrbinary(f"RETR {params.IG_username}.json", f.write)
+    print("wait until complete download 10sec")
+    time.sleep(1)
+    ftp_server.quit()
+    # open and read the file after the overwriting:
+    f = open("session.json", "r")
+    print(f.read())
+    import timeline
+    print(params.IG_username, " if IG timeline its printed means the session is ok")
+    exit()
+
+print("There's not session file local or ftp")
+print("Where are going to log in")
 cl = Client()
-if params.IG_proxy != None:
-    cl.set_proxy(params.IG_proxy)
-    with open('proxy.txt', 'w') as f:
-        f.write(params.IG_proxy)
 cl.login(params.IG_username, params.IG_password)
 cl.dump_settings('session.json')
 print("Datos de la sesión guardados en session.json")
@@ -27,19 +61,17 @@ print(cl.get_settings())
 print("Datos del usuario que inició sesión")
 print(cl.user_info(cl.user_id))
 
-c = open("config.json")
-config = json.load(c)
+
 s = open("session.json")
 session = json.load(s)
 
-ftp_server = ftplib.FTP(config["FTP"]["hostname"],config["FTP"]["username"],config["FTP"]["password"])
-ftp_server.encoding = "utf-8"
-ftp_server.cwd('session_log')
 
 filename = "session.json"
 with open(filename, "rb") as file:
-    ftp_server.storbinary(f"STOR {session['authorization_data']['sessionid']}.json",file)
-
+    ftp_server.storbinary(f"STOR {params.IG_username}.json",file)
+    print("Uploading sesson file")
 ftp_server.dir()
-
 ftp_server.quit()
+
+import timeline
+print(params.IG_username, " if IG timeline its printed means the session is ok")
